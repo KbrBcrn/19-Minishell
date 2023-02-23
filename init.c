57fee5dec4a6eefbbccdca1dc22d54bfa40133ca
Kubra Beceren <kbeceren@student.s19.be>
2023-02-23 13:10:58 +0100

    get_env

--- init.c
+++ init.c
@@ -6,7 +6,7 @@
 /*   By: kbeceren <kbeceren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 12:25:35 by kbeceren          #+#    #+#             */
-/*   Updated: 2023/02/22 12:35:00 by kbeceren         ###   ########.fr       */
+/*   Updated: 2023/02/23 13:09:42 by kbeceren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,3 +26,36 @@ void	ft_init_env(t_data *data, t_list **new_env, char **envp)
 	}
 }
 
+
+
+/* get_shvl is used to obtain the value of the SHLVL environment variable, 
+which indicates the nesting level of the shell process. 
+The value of SHLVL is incremented each time a new shell process is created.
+Function returns a pointer to the value of the environment variable
+(which starts after the equals sign).
+*/
+
+char	*get_shlvl(t_list *env, char *name)
+{
+	char	*content;
+	int		len_name;
+
+	len_name = ft_strlen(name);
+	while (env)
+	{
+		content = (char *) env->content;
+		if (!ft_strncmp(name, content, len_name) && content[len_name] == '=')
+			return (content + len_name + 1); 
+		env = env->next;
+	}
+	return (NULL);
+}
+
+// void	ft_env(t_list *env)
+// {
+// 	while (env)
+// 	{
+// 		printf("%s\n", env->content);
+// 		env = env->next;
+// 	}
+// }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kbeceren <kbeceren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 22:19:21 by kbeceren          #+#    #+#             */
-/*   Updated: 2023/02/22 12:35:05 by kbeceren         ###   ########.fr       */
+/*   Updated: 2023/02/23 13:07:29 by kbeceren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,7 @@ int	main(int argc, char **argv, char **envp)
 
 	ft_memset(&data, 0, sizeof(t_data));
 	ft_init_env(&data, &data.new_env, envp);
+	data.env = increment_shlvl(data.new_env);
 	return (0);
 }
+// get_shlvl(data.new_env, "SHLVL");
